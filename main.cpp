b5522a2ab09adbca0be78fb00f2a01a268da0a0e
valentajulyansaputra <112872050+valentajulyansaputra@users.noreply.github.com>
2022-09-05 20:15:11 +0700

    Pertemuan 1

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     cout << "Nama : Valenta Julyan Saputra" << endl;
     cout << "NIM : A11.2022.14121" << endl;
-    cout << "Teuku Umar demak" << endl;
-    cout << "SMA Negeri 1 Demak" << endl;
+    cout << "Alamat : Teuku Umar demak" << endl;
+    cout << "Asal Sekolah : SMA Negeri 1 Demak" << endl;
     return 0;
 }
