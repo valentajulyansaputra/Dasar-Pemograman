1543eb897cdb9d9fd46263aae4c9133c91314a67
valentajulyansaputra <112872050+valentajulyansaputra@users.noreply.github.com>
2022-09-12 15:36:12 +0700

    Add files via upload

---
+++ pertemuan2/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{       //NAMA LENGKAP
+    cout << "VALENTA JULYAN SAPUTRA" << endl;
+        //NIM (Nomer Induk Mahasiswa)
+    printf("A11.2022.14121");
+    cout <<endl;
+
+    int a = 18;
+    float b =177.8;
+    char c = 'L';
+    string d = "Large";
+
+        cout <<"Umur: " <<a;
+        cout <<endl;
+        cout <<"Tinggi Badan: " <<b;
+        cout <<endl;
+        cout <<"Ukuran Baju: " <<c;
+        cout <<endl;
+        cout <<"Detail Ukuran Baju: " <<d;
+
+    return 0;
+}
